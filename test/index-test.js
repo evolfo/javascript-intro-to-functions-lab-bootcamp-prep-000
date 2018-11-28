721de9820439c79631759c6fb0b744ba73dcb82e
ANGELO SPAMPINATO <spampinatodesign@gmail.com>
2018-11-28 03:37:20 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+ 
+  uppercase.toUpperCase() === uppercase 
+ 
+  var lowercase = 'hello!'
+ 
+  lowercase.toLowerCase() === lowercase 
+ 
+  var iLoveYou = 'I love you, Grandma.'
+ 
+  iLoveYou.toLowerCase() === iLoveYou
+ 
+  iLoveYou.toUpperCase() === iLoveYou
+}
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
